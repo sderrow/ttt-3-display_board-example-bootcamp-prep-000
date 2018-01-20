14864a27508926cdf447d2f5177cc794c496da3b
Sean Derrow <sderrow@mit.edu>
2018-01-20 21:53:34 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,5 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-puts("   |   |   \n-----------\n   |   |   \n-----------\n   |   |   ")
+
+def display_board
+  puts("   |   |   \n-----------\n   |   |   \n-----------\n   |   |   ")
+end
